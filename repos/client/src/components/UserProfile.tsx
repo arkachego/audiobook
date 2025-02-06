8f11193b1c3d862f596bb31cdfe9366abe047120
Arka Halder <arkachego25@Arkas-iMac.local>
2025-02-07 01:12:14 +0530

    refactoring

--- repos/client/src/components/UserProfile.tsx
+++ repos/client/src/components/UserProfile.tsx
@@ -2,6 +2,7 @@ import { UserType } from "@/types";
 import { TextGenerate } from "./ui/text-generate";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import { IconMicrophoneFilled, IconLogout } from "@tabler/icons-react";
 
 type Props = {
   profile: UserType | null;
@@ -11,12 +12,24 @@ const UserProfile: React.FC<Props> = ({ profile }) => {
 
   const router = useRouter();
 
+  const logoutUser = () => {
+    localStorage.removeItem("user_id");
+    router.replace("/onboard");
+  };
+
   return (
     <div className="flex items-center justify-between w-full p-4 border-b-2 border-b-neutral-400">
       <div>
         {profile && <TextGenerate words={`Hi ${profile?.name}!`}/>}
       </div>
-      <Button onClick={() => router.push('/recorder')}>Recorder</Button>
+      <div className="flex gap-2">
+        <Button size="icon" onClick={() => router.push('/recorder')} className="w-8 h-8">
+          <IconMicrophoneFilled/>
+        </Button>
+        <Button size="icon" onClick={logoutUser} className="w-8 h-8">
+          <IconLogout/>
+        </Button>
+      </div>
     </div>
   );
 
